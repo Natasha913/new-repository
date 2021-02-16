1842233b5484a5946682af522e06a3c6900ca06c
natasha <natashakainat91@gmail.com>
2021-02-16 20:44:44 +0600

    UniqueArray

---
+++ Lab3/Lab3/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Student
+    {
+        private string name;
+        private int id;
+        private string department;
+        private float cgpa;
+        public Student()
+        {
+        }
+        public Student(string name, int id, string department, float cgpa)
+        {
+            this.name = name;
+            this.id = id;
+            this.department = department;
+            this.cgpa = cgpa;
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + name);
+
+            Console.WriteLine("ID: " + id);
+
+            Console.WriteLine("Department: " + department);
+
+            Console.WriteLine("CGPA: " + cgpa);
+
+            Console.ReadLine();
+        }
+        public static void Main (string[] arg)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student();
+            s2.name = "Natasha";
+            s2.id = 18-39059-3;
+            s2.ShowInfo();
+        }
+        
+    }   
+ }
+   
+
---
+++ UniqueArray/UniqueArray/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int i, j, k, size, Unique;
+
+            Console.WriteLine("Size of array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter elements : ");
+
+
+            for (i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (i = 0; i < size; i++)
+            {
+                Unique = 1;
+
+                for (j=i+1; j<size; j++)
+                {
+                    if (arr[i] == arr[j])
+                    { 
+                        for(k=j; k < size-1; k++ )
+                        {
+                            arr[k] = arr[k + 1];
+                        }
+                        size--;
+                        j--;
+                        Unique = 0;
+
+                    }
+                }
+
+                if (Unique != 1)
+                {
+                    for (j = i; j < size - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+
+                    size--;
+                    i--;
+                }
+            }
+
+           
+           Console.WriteLine("All unique elements in the array are: "+ size );
+            for (i = 0; i < size; i++)
+            {
+                Console.WriteLine(arr[i] + "\t");
+           }
+            Console.ReadLine();
+        }
+
+    }
+
+} 
+        
+
+       
+    
+
